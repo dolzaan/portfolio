dc77538cb95c458784c3354a8180be71ea0d4a41
dolzaan <oodolzan@gmail.com>
2025-08-06 09:33:18 -0300

    algumas alter

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,43 @@ module.exports = {
     },
   },
   plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        gradient: 'gradient 8s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+      },
+    },
+  },
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        'light-sweep': 'light-sweep 8s ease infinite',
+      },
+      keyframes: {
+        'light-sweep': {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+      },
+    },
+  },
 }
