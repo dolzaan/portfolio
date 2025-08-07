b2c23ef978155aa61de01c4766008ca2cfc6e218
dolzaan <oodolzan@gmail.com>
2025-08-07 17:25:54 -0300

    brincando

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,35 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
       animation: {
-        gradientSlow: "gradient-slow 20s ease infinite",
         'scroll-grid': 'scrollGrid 40s linear infinite',
-        gradient: 'gradient 8s ease infinite',
-        'light-sweep': 'light-sweep 8s ease infinite',
       },
       keyframes: {
-        'gradient-slow': {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-        },
         scrollGrid: {
           '0%': { backgroundPosition: '0 0' },
           '100%': { backgroundPosition: '100px 100px' },
-        },
-        gradient: {
-          '0%, 100%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' },
-        },
-        'light-sweep': {
-          '0%, 100%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' },
         },
       },
     },
